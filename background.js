435541ee3da92339f823cc745d8f834576b91d14
Tariq <tariq1890@yahoo.in>
2017-01-14 01:52:14 -0800

    Implemented Weather API call from the extension

--- background.js
+++ background.js
@@ -2,6 +2,16 @@
 ////////////////////////SET UP ALARM////////////////////
 ////////////////////////////////////////////////////////
 var alarmName = "popup_alarm";
+
+var OpenWeatherConstants = {
+
+    BASE_URL : "http://api.openweathermap.org/data/2.5/weather?",
+    LAT : "lat=",
+    LON : "lon=",
+	APP_KEY : "APPID=e0d69044d0b30f6f989f67de783e0cee"
+
+}
+
 chrome.alarms.create(alarmName,
 {
 	delayInMinutes: 0,
@@ -31,8 +41,12 @@ chrome.alarms.onAlarm.addListener(function(alarm)
 window.addEventListener('unload', function(event)
 {
 	alert("CLOSED")
-});  
+});
 
+window.addEventListener('onload', function(event)
+{
+    console.log("You legged!");
+});
 
 ////////////////////////////////////////////////////////
 ////////////////////////ON ICON CLICKED/////////////////
@@ -40,4 +54,44 @@ window.addEventListener('unload', function(event)
 chrome.browserAction.onClicked.addListener(function(tab)
 {
 	askQuestion();
+
+
+});
+
+chrome.browserAction.onClicked.addListener(function(tab)
+{
+
+    var startPos;
+    var geoOptions = {
+        timeout: 10 * 1000
+    }
+
+    var geoSuccess = function(position) {
+        startPos = position;
+        var url = OpenWeatherConstants.BASE_URL + OpenWeatherConstants.LAT + startPos.coords.latitude + "&" +
+            OpenWeatherConstants.LON + startPos.coords.longitude + "&" + OpenWeatherConstants.APP_KEY;
+        console.log(url);
+        invokeWeatherApi(url);
+
+    };
+    var geoError = function(error) {
+        console.log('Error occurred. Error code: ' + error.code);
+    };
+
+    var invokeWeatherApi = function (url) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                console.log(xhr.responseText);
+            }
+        }; // Implemented elsewhere.
+        xhr.open("GET", url, true);
+        xhr.send();
+    };
+
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+
+
+
+
 });
