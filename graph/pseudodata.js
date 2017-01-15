550cbd4137c97f47ae88de49325b497b646cd3b6
Micah Rutrough <mrutroug@uci.edu>
2017-01-14 17:20:20 -0800

    Added pseudodata and improved graphing

---
+++ graph/pseudodata.js
@@ -0,0 +1,64 @@
+PSEUDO_DATA = '[{\
+	"date":"2016-12-01",\
+	"air_description":"Fair Air Quality",\
+	"air_index":72,\
+	"mood":5,\
+	"temp":58.1,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2016-12-07",\
+	"air_description":"Fair Air Quality",\
+	"air_index":68,\
+	"mood":6,\
+	"temp":69.6,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2016-12-14",\
+	"air_description":"Fair Air Quality",\
+	"air_index":54,\
+	"mood":3,\
+	"temp":56.9,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2016-12-21",\
+	"air_description":"Fair Air Quality",\
+	"air_index":52,\
+	"mood":4,\
+	"temp":60.4,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2016-12-28",\
+	"air_description":"Fair Air Quality",\
+	"air_index":66,\
+	"mood":5,\
+	"temp":62.1,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2017-01-04",\
+	"air_description":"Fair Air Quality",\
+	"air_index":77,\
+	"mood":6,\
+	"temp":68.0,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2017-01-11",\
+	"air_description":"Fair Air Quality",\
+	"air_index":72,\
+	"mood":6,\
+	"temp":71.3,\
+	"weather_description":"few clouds"\
+},\
+{\
+	"date":"2017-01-15",\
+	"air_description":"Fair Air Quality",\
+	"air_index":80,\
+	"mood":6,\
+	"temp":72,\
+	"weather_description":"few clouds"\
+}]'
